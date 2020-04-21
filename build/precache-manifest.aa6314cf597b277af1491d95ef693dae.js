d0f1daa5742184e93b06fb193def24406cb0a66f
darshan405 <dshivakumar@qwinix.io>
2020-04-21 16:00:06 +0530

    reactjs-cicd

---
+++ build/precache-manifest.aa6314cf597b277af1491d95ef693dae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d6d4d4b286b63c77ca2838160616d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16580e322331fbc7f0f5",
+    "url": "/static/css/main.e82b2d60.chunk.css"
+  },
+  {
+    "revision": "2e1161459d751b7d30ab",
+    "url": "/static/js/2.9f16da27.chunk.js"
+  },
+  {
+    "revision": "95bb4b31e9672bbabf6dc98701e0dcac",
+    "url": "/static/js/2.9f16da27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16580e322331fbc7f0f5",
+    "url": "/static/js/main.a07620bf.chunk.js"
+  },
+  {
+    "revision": "f5d63c1fb4a0a5b2095b",
+    "url": "/static/js/runtime-main.52fdbd27.js"
+  },
+  {
+    "revision": "f79a423bc3977deca7759b55e99099be",
+    "url": "/static/media/backgrnd2.f79a423b.jpg"
+  },
+  {
+    "revision": "950ed49a98eb890158039ad29645c1a6",
+    "url": "/static/media/image.950ed49a.jpg"
+  },
+  {
+    "revision": "950ed49a98eb890158039ad29645c1a6",
+    "url": "/static/media/images1.950ed49a.jpg"
+  },
+  {
+    "revision": "3083d0fa676a4b521204cecd8ba9f8b0",
+    "url": "/static/media/misimg.3083d0fa.jpeg"
+  }
+]);
